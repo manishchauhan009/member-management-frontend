e3cdebc2c2be20f76ab4ddc106abfc13b4767512
Manish Chauhan <manishchauhan8401@gmail.com>
2025-03-29 19:45:35 +0530

    added favicon

--- src/admin/AddGallery.js
+++ src/admin/AddGallery.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
-import API_URL from "../Url";
 
 const AddGallery = () => {
     const [title, setTitle] = useState("");
